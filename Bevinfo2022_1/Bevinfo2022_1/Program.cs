1f97f1124671ab510a26ddce17763582dd956e78
Ács Róbert <49874054+ricsirobi@users.noreply.github.com>
2022-11-06 16:52:38 +0100

    done

--- Bevinfo2022_1/Bevinfo2022_1/Program.cs
+++ Bevinfo2022_1/Bevinfo2022_1/Program.cs
@@ -72,11 +72,14 @@ namespace Bevinfo2022_1
             {
                 return false;
             }
-
             for (int i = 0; i < inputString.Length - 1; i++)
             {
                 if (int.TryParse(inputString.Replace(".",""), out int temp2))
                 {
+                    if(inputString.Substring(i,1)==".")
+                    {
+                        i++;
+                    }
                     if (int.Parse(inputString.Substring(i, 1)) > 7 || int.Parse(inputString.Substring(i, 1)) < 0)
                     {
                         return false;
@@ -186,25 +189,12 @@ namespace Bevinfo2022_1
             CowSay("HEX: {" + resultHex + "}");
 
         }
-        public static void exchangeFromHex()
-        {
-            //
-            CowSay("Írj be egy 16-os számrendszerbeli számot");
-            inputString = Console.ReadLine();
-            while (!isHexNumber())
-            {
-                CowSay("Próbáld újra");
-                inputString = Console.ReadLine();
-
-            }//
-
-            CowSay("MOST FOGOK VÁLTANI 16-osból");
-        }
+        
         public static string exchangeFromOctToBin(string input)
         {
             string result = "";
             bool toBreak = false;
-                int stopped = -1;
+                int stopped = input.Length;
             for (int i = 0; i < input.Length; i++)
             {
                 
@@ -226,8 +216,39 @@ namespace Bevinfo2022_1
                     break;
                 }
             }
-
-            Console.WriteLine("Pontig megvan: " + result + "." +input.Substring(stopped)+ "(a pont utáni még ninc sváltva)") ;
+            
+            if(stopped==input.Length)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                result += "....";
+                string resultOtherHalf="0.";
+                
+                for (int i = stopped; i < input.Length; i++)
+                {
+                    resultOtherHalf += input.Substring(i, 1);
+                }
+                double resultHalfDouble = Double.Parse(resultOtherHalf);
+                Console.WriteLine("RESUT HALF:\n" + resultHalfDouble);
+                while (resultHalfDouble!=1 && resultHalfDouble.ToString().Length>=21)
+                {
+                    resultHalfDouble *= 2;
+                    if(resultHalfDouble>1)
+                    {
+                        resultHalfDouble -= 1;
+                        result += "1";
+                    }
+                    else
+                    {
+                        result += "0";
+                    }
+                    Console.WriteLine(resultHalfDouble);
+                }
+                result += "1";
+                Console.WriteLine("Pontig megvan: " + result  + "(a pont utáni még ninc sváltva)");
+            }
 
 
             return result;
@@ -236,6 +257,33 @@ namespace Bevinfo2022_1
         {
             string result = "";
             List<string> list = new List<string>();
+            if(input.Contains('.'))
+            {
+                string inputSecondHalf="";
+                if (input.Replace("....", ".").Split('.')[0].Length % 4 != 0)
+                {
+                    for (int i = 0; i < input.Replace("....", ".").Split('.')[0].Length % 4; i++)
+                    {
+                        input = "0" + input;
+                    }
+                    Console.WriteLine("INPUTOM: " + input);
+                }
+
+                if (input.Replace("....", ".").Split('.')[1].Length % 4 != 0)
+                {
+                    inputSecondHalf = input.Replace("....", ".").Split('.')[1];
+
+                    Console.WriteLine("BEJOTTEM"+ "szám: "+inputSecondHalf + " maradék:" + (4-inputSecondHalf.Length));
+                    for (int i = 0; i < inputSecondHalf.Length % 4; i++)
+                    {
+                        Console.WriteLine("forban vagyok: "+i);
+                        inputSecondHalf = inputSecondHalf+"0";
+                    }
+                    input = input.Replace("....", ".").Split('.')[0] + "...." + inputSecondHalf;
+                    Console.WriteLine("INPUTOM: " + input);
+                }
+            }
+            
             for (int i = 0; i < input.Length - 3; i += 4)
             {
                 //CowSay("forban vagyok, i=" + i);
@@ -257,6 +305,7 @@ namespace Bevinfo2022_1
                     case "1101": list.Add("D"); break;
                     case "1110": list.Add("E"); break;
                     case "1111": list.Add("F"); break;
+                    case "....":list.Add("."); break;
 
                     default: list.Add("0"); break;
                 }
@@ -269,7 +318,112 @@ namespace Bevinfo2022_1
             return result;
         }
 
+        public static void exchangeFromHex()
+        {
+            //
+            CowSay("Írj be egy 16-os számrendszerbeli számot");
+            inputString = Console.ReadLine();
+            while (!isHexNumber())
+            {
+                CowSay("Próbáld újra");
+                inputString = Console.ReadLine();
 
+            }//
+            Console.WriteLine("(:--------------------------------------------------------------:)");
+            CowSay("HEX: {" + inputString + "}");
+            Console.WriteLine("////////////////////////////////////////////////////////");
+            Console.WriteLine("HEX  TO  BIN");
+            Console.WriteLine("////////////////////////////////////////////////////////");
+            //PÉLDA
+            //1A -> 1 1010
+            //12345 -> 110110110 110110... 1010110 110110110 3-assval váltjuk egyenként 
+
+
+            string resultBin = exchangeFromHexToBin(inputString);
+            CowSay("BIN: {" + resultBin + "}");
+
+            Console.WriteLine("////////////////////////////////////////////////////////");
+            Console.WriteLine("BIN  TO  OCT");
+            Console.WriteLine("////////////////////////////////////////////////////////");
+
+            switch (resultBin.Length % 3)
+            {
+                case 1: resultBin = "00" + resultBin; break;
+                case 2: resultBin = "0" + resultBin; break;
+                default: break;
+            }
+            string resultOct = exchangeFromBinToOct(resultBin);
+            CowSay("OCT: {" + resultOct + "}");
+
+        }
+        public static string exchangeFromHexToBin(string input)
+        {
+            string result = "";
+            bool toBreak = false;
+            int stopped = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                switch (input.Substring(i, 1).ToUpper())
+                {
+                    case "0": result += "0000"; break;
+                    case "1": result += "0001"; break;
+                    case "2": result += "0010"; break;
+                    case "3": result += "0011"; break;
+                    case "4": result += "0100"; break;
+                    case "5": result += "0101"; break;
+                    case "6": result += "0110"; break;
+                    case "7": result += "0111"; break;
+                    case "8": result += "1000"; break;
+                    case "9": result += "1001"; break;
+                    case "A": result += "1010"; break;
+                    case "B": result += "1011"; break;
+                    case "C": result += "1100"; break;
+                    case "D": result += "1101"; break;
+                    case "E": result += "1110"; break;
+                    case "F": result += "1111"; break;
+                    case ".": toBreak = true; stopped = i + 1; break;
+                    default: result += "0000"; break;
+                }
+                if (toBreak)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Pontig megvan: " + result + "." + input.Substring(stopped) + "(a pont utáni még nincs váltva)");
+
+
+            return result;
+        }
+        public static string exchangeFromBinToOct(string input)
+        {
+            string result = "";
+            List<string> list = new List<string>();
+            for (int i = 0; i < input.Length - 2; i += 3)
+            {
+                //CowSay("forban vagyok, i=" + i);
+                switch (input.Substring(i, 3))
+                {
+                    case "000": list.Add("0"); break;
+                    case "001": list.Add("1"); break;
+                    case "010": list.Add("2"); break;
+                    case "011": list.Add("3"); break;
+                    case "100": list.Add("4"); break;
+                    case "101": list.Add("5"); break;
+                    case "110": list.Add("6"); break;
+                    case "111": list.Add("7"); break;
+
+
+                    default: list.Add("0"); break;
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                result += list[i];
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
             chooseChangeType();
